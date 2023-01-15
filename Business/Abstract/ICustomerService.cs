0ef7c74b3efebb52e3a927c18bf777918ec3f105
Aziz <a.azzylmz@gmail.com>
2023-01-15 17:07:29 +0300

    Bussines katmanına Customer servisi eklendi

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+ public  interface ICustomerService
+    {
+
+        bool Add(Customer customer);
+        IEnumerable<Customer> GetAll();
+    }
+}
---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+   public class CustomerManager :ICustomerService
+    {
+        ICustomerDal _customerDal;
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public bool Add(Customer customer)
+        {
+            bool result;
+            try
+            {
+                _customerDal.Add(customer);
+                result = true;
+            }
+            catch (Exception ee)
+            {
+                result = false;
+
+            }
+            return result;
+        }
+        public IEnumerable<Customer> GetAll()
+        {
+            return _customerDal.GetAll();
+        }
+    }
+}
